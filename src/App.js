16170e4f33e95607c029882b7f67feca37fda92a
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2023-11-14 21:22:51 +0900

    過去の着手の表示

--- src/App.js
+++ src/App.js
@@ -50,13 +50,24 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    const description = move > 0 ?
+      'Go to move #' + move :
+      'Go to game start';
+    return <li><button onClick={() => jumpTo(move)}>{description}</button></li>
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
