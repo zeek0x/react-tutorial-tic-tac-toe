5e9460d9a40fe1b8931bdc325c24a7d914d1a18c
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2023-11-14 21:29:17 +0900

    タイムトラベルの実装

--- src/App.js
+++ src/App.js
@@ -43,22 +43,30 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 == 0);
   }
 
   const moves = history.map((squares, move) => {
     const description = move > 0 ?
       'Go to move #' + move :
       'Go to game start';
-    return <li><button onClick={() => jumpTo(move)}>{description}</button></li>
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
   });
 
   return (
